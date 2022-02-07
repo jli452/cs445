3993d5f990a053b87706602530d3b80b0df09d9e
Jin Xian Li <jli452@binghamton.edu>
2022-02-07 18:56:51 -0500

    crude version fix later

--- pyEncrypt/app.py
+++ pyEncrypt/app.py
@@ -1,7 +1,8 @@
 from cryptography.fernet import Fernet
-from flask import Flask
+from flask import *
 import base64
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'woohoo'
 
 class EncryptionStr:
     def __init__(self, encrypt, s):
@@ -12,18 +13,22 @@ def base64_encrypt(estr):
     encodedBytes = base64.b64encode(estr.s.encode("utf-8"))
     estr.s = str(encodedBytes, "utf-8")
 
-def main():
-    estr = EncryptionStr(base64_encrypt, "hello world")
-    print(estr.s)
-    estr.encrypt(estr)
-    print(estr.s)
-
-if __name__ == "__main__":
-    main()
-
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return render_template('index.html')
+
+@app.route('/update', methods=('GET', 'POST'))
+def update():
+    if request.method == 'POST':
+        str = request.form['str']
+        estr = EncryptionStr(base64_encrypt, str)
+        estr.encrypt(estr)
+        return estr.s
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
 
 #build using "docker build -t getting-started ."
 #then run the image with "docker run --publish 5000:5000 getting-started" or any port
